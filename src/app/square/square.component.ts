514778c7f9546725630075230062de20c74dccfd
nuri1977 <nurilacka@gmail.com>
2020-05-31 02:55:45 +0200

    final commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O';
+
+}
